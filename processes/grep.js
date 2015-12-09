b843a68e618cf17454f0845b560b4210e4dc06d6
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-12-09 16:53:48 +0300

    data from one process to another

---
+++ processes/grep.js
@@ -0,0 +1,43 @@
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 09.12.2015
+ * Time: 16:51
+ */
+
+// Использование дочерних процессов для поиска файлов в подкаталогах по заданному аргументу test
+var spawn = require('child_process').spawn,
+    find = spawn('find',['.','-ls']),
+    grep = spawn('grep',['test']);
+grep.stdout.setEncoding('utf8');
+// направление результатов поиска в адрес grep
+find.stdout.on('data', function(data) {
+    grep.stdin.write(data);
+});
+// а теперь запуск grep и вывод результатов
+grep.stdout.on('data', function (data) {
+    console.log(data);
+});
+// обработка ошибки для обоих процессов
+find.stderr.on('data', function (data) {
+    console.log('grep stderr: ' + data);
+});
+grep.stderr.on('data', function (data) {
+    console.log('grep stderr: ' + data);
+});
+// и завершение обработки для обоих процессов
+find.on('exit', function (code) {
+    if (code !== 0) {
+        console.log('find process exited with code ' + code);
+    }
+// продолжение обработки и завершение процесса grep
+    grep.stdin.end();
+});
+grep.on('exit', function (code) {
+    if (code !== 0) {
+        console.log('grep process exited with code ' + code);
+    }
+});
