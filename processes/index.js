8b19902de8434bddba03ce5b5a2c88465778bfca
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-12-09 16:47:33 +0300

    spawn process

---
+++ processes/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 09.12.2015
+ * Time: 16:46
+ */
+
+var spawn = require('child_process').spawn,
+    pwd = spawn('pwd');
+
+pwd.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+pwd.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+pwd.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+});
