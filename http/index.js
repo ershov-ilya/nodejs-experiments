432f7b90962a048cb8c87b73dc787753596f7d99
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-12-10 13:24:54 +0300

    process.exit(0);

--- http/index.js
+++ http/index.js
@@ -14,8 +14,21 @@ const hostname = '127.0.0.1';
 const port = 1337;
 
 http.createServer((req, res) => {
+    if(req.url=='/favicon.ico') return false;
+    console.log('Connection '+req.url);
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    if(req.url=='/stop') {
+        res.end('Stop command\n');
+        process.exit(0);
+    }
     res.end('Hello World\n');
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+process.on('SIGTERM', function () {
+    console.log("Closing");
+    http.close();
 });
