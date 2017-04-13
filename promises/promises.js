49a063f87ccc13c905ab2e68dad531f9500d506a
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-04-13 12:29:55 +0300

    nodejs promisses

--- promises/promises.js
+++ promises/promises.js
@@ -1,6 +1,4 @@
-﻿var colors = require('colors');
-
-console.log('start');
+﻿console.log('start');
 
 process.state={value: 0};
 // Создаётся объект promise
@@ -8,8 +6,9 @@ let promise = new Promise((resolve, reject) => {
 	
 	setTimeout(() => {
 		// переведёт промис в состояние fulfilled с результатом "result"
-		// reject(new Error("время вышло!"));
-		resolve("result");
+		reject("Текст Error");
+		resolve("значение результата");
+		reject(new Error("Объект Error"));
 	}, 1000);
 	
 });
@@ -19,11 +18,11 @@ promise
 	.then(
 		result => {
 			// первая функция-обработчик - запустится при вызове resolve
-			console.log("Fulfilled: ".green, result); // result - аргумент resolve
+			console.log('\x1b[33m%s\x1b[0m',"Fulfilled: ", result); // result - аргумент resolve
 		},
 		error => {
 			// вторая функция - запустится при вызове reject
-			console.log("Rejected: ".red, error); // error - аргумент reject
+			console.log('\x1b[31m%s\x1b[0m',"Rejected: ", error); // error - аргумент reject
 		}
 	);
 console.log('end');
